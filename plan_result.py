5229ad0ecd574f074ff9c44639cacf2146697475
natalia-nd <nataliadarmaja93@gmail.com>
2017-11-01 23:57:50 +0800

    commit plan_result.py

---
+++ plan_result.py
@@ -0,0 +1,10 @@
+def result(tree):
+    #operation_name = tree.get_attr("Node Type")
+    plan_rows = tree.get_attr("Plan Rows")
+    msg = "The DBMS obtains {} rows as a result".format(plan_rows)
+    
+    for child in tree.children:
+        msg += child.explain()
+        
+    return msg
+    
