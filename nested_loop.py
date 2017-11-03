bc6e65c26fd1b8f54af7c47b09ff4e073f00412c
andreculham <andre.culham@gmail.com>
2017-11-04 00:25:31 +0700

    Fixed nested loop

--- nested_loop.py
+++ nested_loop.py
@@ -1,10 +1,12 @@
 import re
 def nested_loop(planTree):
     node = planTree.get_attr("Node Type")
-    tablename = planTree.get_attr("Join Filter")
-    cond = re.split('=' , tablename)
-    description = "The DBMS performs {} on two tables by fetching the result from {} and querying the table {} for each row from the first.".format(
-        node, cond[0], con[1])
+    join_type = planTree.get_attr("Join Type")
+    join_filter = planTree.get_attr("Join Filter")
+    if join_filter != '':
+        join_filter = ' with join filter '+ join_filter
+    description = "The DBMS performs {} {} join{}. ".format(
+        node, join_type, join_filter)   
     childStr = ""
     for child in planTree.children:
         childStr += child.explain()
