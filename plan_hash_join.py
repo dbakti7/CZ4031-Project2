86b7f559dddcd03605d7a7724195fabdb982fd50
natalia-nd <nataliadarmaja93@gmail.com>
2017-11-04 01:11:56 +0800

    update on hash_join and plan_tree.py

--- plan_hash_join.py
+++ plan_hash_join.py
@@ -8,18 +8,18 @@ def hash_join (tree):
 
     operation_name = tree.get_attr("Node Type")
     operation_type = tree.get_attr("Join Type")
-    cond_msg = parse_cond(tree.get_attr("Hash Cond"))
-    rows_result = tree.get_attr("Actual Rows")
-    table_name = parse_table_name(cond_msg)
+    cond_msg = parse_cond(tree.get_attr("Hash Cond").strip('()'))
+    rows_result = tree.get_attr("Plan Rows")
+    table_name = parse_table_name(tree.get_attr("Hash Cond").strip('()').replace(' ', ''))
     
-    msg1 = "The DBMS performs {} {} on table {}.".format (operation_type, operation_name, table_name)
+    msg1 = "The DBMS performs {} {} on table {}.\n".format (operation_type, operation_name, table_name)
     if (operation_type == "Anti"):
-        msg2 = "The join occurs where not exist {}".format(cond_msg)
+        msg2 = "The join occurs where not exist {}.\n".format(cond_msg)
     elif (operation_type == ""):
-        msg2 = "The join condition is {}".format(cond_msg)
+        msg2 = "The join condition is {}.\n".format(cond_msg)
     else:
         msg2 = ""
-    msg3 = "The join result consist of {} rows".format(rows_result)
+    msg3 = "The join result consists of {} rows.\n".format(rows_result)
     
     msg = msg1 + msg2 + msg3
     
@@ -37,23 +37,23 @@ def parse_cond (cond):
 
 def parse_table_name (cond_msg):
     tableName = []
-    for word in cond_msg.split():
-        dot = word.find(".")
-        if (dot>0):
-            name = word[0: (dot)]
-            if name not in tableName:
-                tableName.append(name)
-    end = len(tableName) - 1
-    if tableName == []:
-        return ""
-    elif len(tableName) == 1:
-        string = tableName[0]
-        return string
-    else:
-        string = tableName[0]
-        for num in range(1,end):
-            string += " and " + tableName[num]
-        return string
+    #table_name = ""
+    cond_list = cond_msg.split("=")
+    for word in cond_list:
+        dot = word.find('.')
+        word_new = word[0:int(dot)]
+        tableName.append(word_new)
+    return ' and '.join(tableName)
+#    if len(tableName) == 0:
+#        return table_name
+#    elif len(tableName) == 1:
+#        table_name = tableName[0]
+#        return table_name
+#    else:
+#        table_name = tableName[0]
+#        for num in range(1,len(tableName) - 1):
+#            table_name += " and " + tableName[num]
+#        return table_name
 
     
         
