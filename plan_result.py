86131004d926f8ee79076032d93aa05f79818c06
natalia-nd <nataliadarmaja93@gmail.com>
2017-11-04 01:22:31 +0800

    add end line on each sentence

--- plan_result.py
+++ plan_result.py
@@ -1,7 +1,7 @@
 def result(tree):
     #operation_name = tree.get_attr("Node Type")
     plan_rows = tree.get_attr("Plan Rows")
-    msg = "The DBMS obtains {} rows as a result".format(plan_rows)
+    msg = "The DBMS obtains {} rows as a result the query.\n".format(plan_rows)
     
     for child in tree.children:
         msg += child.explain()
